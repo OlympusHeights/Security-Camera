9f784e73e9af11294ea907fc09b8bf717ad6206b
OlympusHeights <40605844+OlympusHeights@users.noreply.github.com>
2018-08-31 12:30:07 -0700

    description maker

---
+++ make_description.py
@@ -0,0 +1,17 @@
+#create_description_files
+
+def create_pos_n_neg():
+	for file_type in ['neg']:
+		for img in os.listdir(file_type):
+			if file_type == 'neg':
+				line = file_type+'/'+img+'\n'
+				with open('bg.txt', 'a') as f:
+					f.write(line)
+
+			elif file_type == 'pos':
+				line = file_type + '/'+img_'1 0 0 50 50\n'
+				with open('info.dat', 'a') as f:
+					f.write(line)
+		
+
+create_pos_n_neg()
